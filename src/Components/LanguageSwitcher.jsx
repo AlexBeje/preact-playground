f3de0c8ba52af12a8af592a5cd694d90e047e90e
AlexBeje <alex.bejenaru93@gmail.com>
2022-03-09 21:19:49 +0100

    3 files modified

--- src/Components/LanguageSwitcher.jsx
+++ src/Components/LanguageSwitcher.jsx
@@ -1,21 +1,32 @@
 // Translations
 import { useTranslation } from 'react-i18next';
 
-// Mantine
+// Mantine Hooks
+import { useLocalStorageValue } from '@mantine/hooks/';
+
+// Mantine Components
 import { Button } from '@mantine/core';
 
 export function LanguageSwitcher() {
+  const [localStorageLanguage, setlocalStorageLanguage] = useLocalStorageValue({
+    key: 'i18nextLng',
+    default: 'en',
+  });
   const { i18n } = useTranslation();
 
   const switchLanguage = () => {
-    i18n.language === 'en'
-      ? i18n.changeLanguage('es')
-      : i18n.changeLanguage('en');
+    if (localStorageLanguage === 'en') {
+      setlocalStorageLanguage('es');
+      i18n.changeLanguage('es');
+    } else {
+      setlocalStorageLanguage('en');
+      i18n.changeLanguage('en');
+    }
   };
 
   return (
     <Button onClick={() => switchLanguage()}>
-      {i18n.language === 'en' ? 'EN' : 'ES'}
+      {localStorageLanguage.toUpperCase()}
     </Button>
   );
 }
